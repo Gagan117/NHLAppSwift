8e6e8959433d0640744dedcd43ef07b8c52e1bd9
Gaganvir Bhullar <bhgaganv@sheridancollege.ca>
2026-01-16 13:48:15 -0500

    Initial Commit

---
+++ NHLApp/NHLAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NHLAppApp.swift
+//  NHLApp
+//
+//  Created by Gaganvir Bhullar on 2026-01-16.
+//
+
+import SwiftUI
+
+@main
+struct NHLAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NHLAppUITests/NHLAppUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  NHLAppUITestsLaunchTests.swift
+//  NHLAppUITests
+//
+//  Created by Gaganvir Bhullar on 2026-01-16.
+//
+
+import XCTest
+
+final class NHLAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
